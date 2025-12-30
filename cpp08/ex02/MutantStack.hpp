3b103dc6654ad85ad7a1be60380e87568c7fb6d7
styx-trav <naomi.rakotomanga@gmail.com>
2025-12-30 13:09:30 +0100

    should be all done ? more tests pending maybe

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -10,11 +10,13 @@
 template <class T, class Container = std::deque<T> > class MutantStack
 {
   private:
-    T arr;
+    Container arr;
   public:
+    //constructors/destructor
     MutantStack() {std::cout << "creating MutantStack\n";};
     ~MutantStack(){std::cout << "destroying MutantStack\n";};
-    MutantStack(MutantStack &other){arr = other.arr; std::cout << "creating (copy) MutantStack\n";};
+    MutantStack(const Container &other) : arr(other) {std::cout << "creating (from container) MutantStack\n";};
+    MutantStack(MutantStack &other) : arr(other.arr){std::cout << "creating (copy) MutantStack\n";};
     MutantStack &operator=(MutantStack &other)
     {
       if (this != &other)
@@ -24,11 +26,35 @@ template <class T, class Container = std::deque<T> > class MutantStack
       }
       return *this;
     };
+    
+    //iterators
+    typedef typename Container::iterator iterator;
+    typedef typename Container::reverse_iterator reverse_iterator;
+    //iterator init
+    inline iterator begin() {return arr.begin();};
+    inline reverse_iterator rbegin() {return arr.rbegin();};
+    inline iterator end() {return arr.end();};
+    inline reverse_iterator rend() {return arr.rend();};
+    
+    //const iterators
+    typedef typename Container::const_iterator const_iterator;
+    typedef typename Container::const_reverse_iterator const_reverse_iterator;
+    //const iterator init
+    inline const_iterator begin() const {return arr.begin();};
+    inline const_reverse_iterator rbegin() const {return arr.rbegin();};
+    inline const_iterator end() const {return arr.end();};
+    inline const_reverse_iterator rend() const {return arr.rend();};
+    
+    //stack-spec functions
     unsigned int size() const {return arr.size();};
     bool empty() const {return arr.empty();};
-    void push(T &item) {arr.push_back(item);};
+    void push(T item) {arr.push_back(item);};
     void pop() {arr.pop_back();};
     T &top() {return arr.back();};
-}
+    
+    //added prints
+    void print() {for (typename Container::iterator it = arr.begin(); it != arr.end(); it++) {std::cout << *it << ". ";} std::cout << std::endl;};
+    void print() const {for (typename Container::const_iterator it = arr.begin(); it != arr.end(); it++) {std::cout << *it << ". ";} std::cout << std::endl;};
+};
 
 #endif
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,35 @@
+#include "MutantStack.hpp"
+#include <cstring>
+
+int main()
+{
+  MutantStack<int> letsee;
+  letsee.push(7);
+  letsee.push(5);
+  letsee.push(4);
+  letsee.push(5);
+  letsee.push(6);
+  letsee.push(0);
+  letsee.print();
+  std::cout << std::endl;
+  
+  const MutantStack<int> letsee2(letsee);
+  letsee2.print();
+  MutantStack<int>::const_iterator it = letsee2.begin();
+  MutantStack<int>::const_reverse_iterator rit = letsee2.rbegin();
+  it++;
+  it++;
+  rit++;
+  std::cout << "and the third :: " << *it << " and one to last :: " << *rit << std::endl << std::endl;
+  
+  std::vector<std::string> cont;
+  cont.push_back("here");
+  cont.push_back("some");
+  cont.push_back("more");
+  cont.push_back("words");
+  MutantStack<std::string, std::vector<std::string> > letsee3(cont);
+  letsee3.print();
+  MutantStack<std::string, std::vector<std::string> >::reverse_iterator ritt = letsee3.rbegin();
+  ritt++;
+  std::cout << "and one to last :: " << *ritt << std::endl << std::endl;
+}
