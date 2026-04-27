00fd1993e4480f1eb0a149d44f2a1065deac4b83
styx-trav <naomi.rakotomanga@gmail.com>
2026-04-27 16:40:18 +0200

    corrected equation + simplified binary sort

--- cpp09/ex02/phase_one.cpp
+++ cpp09/ex02/phase_one.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <cstring>
 #include <sstream>
+#include <cmath>
 
 typedef std::vector<std::pair<std::vector<unsigned int>,std::vector<unsigned int> > > s_list;
 typedef std::vector<unsigned int> a_list;
+
+void print_res(s_list &res, std::vector<a_list> &leftovers);
 
 int comps = 0;
 
@@ -28,6 +31,7 @@ s_list phase_one(s_list l, std::vector<a_list> &lo)
 		}
 		a_list tmp1;
 		a_list tmp2;
+		comps++;
 		if (it->second.back() < prev->second.back()) {
 			for (a_list::iterator itt = prev->first.begin(); itt != prev->first.end(); itt++)
 				tmp2.push_back(*itt);
@@ -123,21 +127,10 @@ void makePair(s_list &to_add, a_list &add, unsigned int index)
 	to_add.insert((to_add.begin() + index), std::pair<a_list, a_list>(tmp1, tmp2));
 }
 
-unsigned int get_js(unsigned int n) //hardcoded for now, equation later
+unsigned int get_js(unsigned int n) //equation, approximated
 {
-	switch (n) {
-		case 1 :
-			return 3;
-		case 2 :
-			return 5;
-		case 3 :
-			return 11;
-		case 4 :
-			return 21;
-		default :
-			return 100;
-	}
-	return 0;
+	double s = (pow(2, n+1) + pow(-1, n)) / 3;
+	return static_cast<unsigned int>(s);
 }
 
 bool cmp(unsigned int b, unsigned int a) { comps++; return (b <= a); }
@@ -147,40 +140,31 @@ void binary_insert(s_list &res, a_list &lo, unsigned int end)
 	if (!end) { end = res.size(); }
 	unsigned int beg = 0;
 	unsigned int pos;
-	unsigned int i = 0;
 	while (true) {
-		pos = beg + (end - beg) /2 + (end- beg) % 2;
-		if (beg == end) { std::cout << "!!!!! beg is end\n"; }
-		std::cout << "scouring " << beg << " to " << end << ", pos " << pos << std::endl;
-		unsigned int prev = res[pos -1].second.back();
-		if (pos == res.size()) {
-			if (cmp(prev, lo.back())) { makePair(res, lo); }
-			else { makePair(res, lo, pos -1); }
-			return ;
-		}
-		unsigned int next = res[pos].second.back();
-		std::cout << "comparing " << prev << " to " << lo.back() << " to " << next << std::endl;
-		if (!cmp(lo.back(), next)) {
-			if (pos != end) { beg = pos; }
-			else { makePair(res, lo, pos + 1) ; return ;}
-		}
-		else if (!cmp(prev, lo.back())) {
-			if (pos-1 && pos-1 != beg) { end = pos -1; }
-			else { makePair(res, lo, pos -1); return ; }
-		}
-		else { break ; }
+		pos = beg + (end - beg) /2;
+		//if (beg == res.size()) { std::cout << "beg == size(), adding\n"; break ; }
+		if (pos == end) { std::cout << "pos at the end of the list ! adding " << lo.back(); break ; }
+		std::cout << "scouring " << beg << ":" << res[beg].second.back() << " to " << end;
+		if (end == res.size()) { std::cout << ":end"; }
+		else
+			std::cout << ":" << res[end].second.back();
+		std::cout << ", pos " << pos << " for " << lo.back() << std::endl;
+		unsigned int compare = res[pos].second.back();
+		if (!cmp(lo.back(), compare)) { beg = pos +1; }
+		else if (beg == pos) { break ; }
+		else { end = pos; }
+		
 	}
+	std::cout << "adding " << lo.back() << ", in between\n";
 	makePair(res, lo, pos);
 }
 
-unsigned int get_a_pos(s_list &res, unsigned int a, unsigned int min)
+unsigned int get_a_pos(s_list &res, unsigned int a)
 {
-	unsigned int i = res.size() - min -1;
-	std::cout << "currently seeking value " << a << " in res, from " << i;
+	unsigned int i = res.size() -1;
 	while (i && res[i].second.back() >= a)
 		i--;
-	std::cout << ", result, " << i << std::endl;
-	return i;
+	return ++i;
 }
 
 s_list phase_two(s_list l, std::vector<a_list> &lo)
@@ -198,19 +182,21 @@ s_list phase_two(s_list l, std::vector<a_list> &lo)
 		for (unsigned int i = prev; i != js; i++)
 			makePair(res, l[i].second);
 		for (unsigned int i = js -1; i >= prev; i--)
-			binary_insert(res, l[i].first, get_a_pos(res, l[i].second.back(), i - prev));
+			binary_insert(res, l[i].first, get_a_pos(res, l[i].second.back()));
 		prev = js;
 	}
 	if (prev != l.size()) {
 		for (unsigned int i = prev; i != l.size(); i++)
 			makePair(res, l[i].second);
 		for (unsigned int i = l.size() -1; i >= prev; i--)
-			binary_insert(res, l[i].first, get_a_pos(res, l[i].second.back(), i - prev));
+			binary_insert(res, l[i].first, get_a_pos(res, l[i].second.back()));
 	}
 	if (!lo.empty() && lo.back().size() == l.front().first.size()) {
 		binary_insert(res, lo.back(), 0);
 		lo.erase(--lo.end());
 	}
+	//print_res(res, lo);
+	//std::cout << "\n-------------------------------------------\n";
 	if (!(res.front().first.empty())) { return phase_two(res, lo); }
 	return res;
 }
@@ -231,14 +217,16 @@ void setup(std::string str, std::vector<unsigned int> &ref)
 {
 	std::stringstream s(str);
 	std::string current;
-	getline(s, current, '\n');
-	while (!s.eof() && !current.empty()) {
+	while (!s.eof()) {
+		getline(s, current);
+		if (current.empty()) { continue ; }
 		int n = std::atoi(current.c_str());
 		if (n < 0) { std::cerr << "negative number !\n"; throw std::exception(); }
-		getline(s, current, '\n');
 		ref.push_back(n);
 	}
 }
+
+bool verify(s_list &res);
 
 int main(int argc, char **argv)
 {
@@ -248,6 +236,18 @@ int main(int argc, char **argv)
 	catch (std::exception &e) { return 0; }
 	std::vector<std::vector<unsigned int> > leftovers;
 	s_list res = phase_one(nerf, leftovers);
+	print_res(res, leftovers);
+	std::cout << "phase two starts ------------------------------------------------------------------\n";
+	res = phase_two(res, leftovers);
+	std::cout << "phase two over --------------------------------------------------------------------\n";
+	print_res(res, leftovers);
+	std::cout << "is list ordered ? " << verify(res) << std::endl;
+	std::cout << "show me max comparisons for " << nerf.size() << " : " << F(nerf.size()) << " v our amount " << comps << std::endl;
+	return 0;
+}
+
+void print_res(s_list &res, std::vector<a_list> &leftovers)
+{
 	int i = 1;
 	for (s_list::iterator it = res.begin(); it != res.end(); it++) {
 		std::cout << "b" << i << " { ";
@@ -269,30 +269,14 @@ int main(int argc, char **argv)
 		}
 		std::cout << "\n}\n";
 	}
-	std::cout << "phase two pt1 starts ------------------------------------------------------------------\n";
-	res = phase_two(res, leftovers);
-	std::cout << "phase two pt1 over --------------------------------------------------------------------\n";
-	i = 1;
-	for (s_list::iterator it = res.begin(); it != res.end(); it++) {
-		std::cout << "b" << i << " { ";
-		for (std::vector<unsigned int>::iterator itt = it->first.begin(); itt != it->first.end(); itt++)
-			std::cout << *itt << ", ";
-		std::cout << "}, a" << i << " { ";
-		for (std::vector<unsigned int>::iterator itt = it->second.begin(); itt != it->second.end(); itt++)
-			std::cout << *itt << ", ";
-		std::cout << "}\n";
-		i++;
+}
+
+bool verify(s_list &res)
+{
+	s_list::iterator prev = res.begin();
+	for (s_list::iterator it = ++res.begin(); it != res.end(); it++) {
+		if (it->second.back() < prev->second.back()) { return false; }
+		prev = it;
 	}
-	if (!leftovers.empty()) {
-		std::cout << "leftovers : { ";
-		for (std::vector<std::vector<unsigned int> >::iterator it = leftovers.begin(); it != leftovers.end(); it++) {
-			std::cout << "\n\t{ ";
-			for (std::vector<unsigned int>::iterator itt = it->begin(); itt != it->end(); itt++)
-				std::cout << *itt << ", ";
-			std::cout << "}";
-		}
-		std::cout << "\n}\n";
-	}
-	std::cout << "show me max comparisons for " << nerf.size() << " : " << F(nerf.size()) << " v our amount " << comps << std::endl;
-	return 0;
+	return true;
 }
