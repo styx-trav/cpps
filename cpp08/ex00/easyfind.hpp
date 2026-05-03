875f3d9983f10c7e186adfd4c6e62cff06862ba8
styx-trav <naomi.rakotomanga@gmail.com>
2026-05-03 22:27:00 +0200

    better ex00 testing

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -2,9 +2,19 @@
 # define EASYFIND_HPP
 # include <bits/stdc++.h>
 
-template <typename T> int &easyfind(T &l, int num)
+template <typename T> int &easyfind(T &l, const int num)
 {
   for (typename T::iterator it = l.begin(); it != l.end(); it++)
+  {
+    if (*it == num)
+      return *it;
+  }
+  throw std::exception();
+}
+
+template <typename T> const int &easyfind(const T &l, const int num)
+{
+  for (typename T::const_iterator it = l.begin(); it != l.end(); it++)
   {
     if (*it == num)
       return *it;
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -14,10 +14,25 @@ int main()
     obj.at(5) = 4;
     //{5, 7, 4, 6, 5, 4};
     //finding 4;
-    std::cout << "looking for "4" in list {5 7 4 6 5 4} : ";
+    std::cout << "looking for \"4\" in vector {5 7 4 6 5 4} : ";
     try
     {
-      if (&(easyfind<std::vector<int> >(obj, 11)) == &(obj.at(2)))
+      if (&(easyfind<std::vector<int> >(obj, 4)) == &(obj.at(2)))
+        std::cout << "found it !\n";
+      else
+        std::cout << "wrong reference returned !\n";
+    }
+    catch (std::exception &e)
+    {
+      std::cout << "couldn't find that\n";
+    }
+    const std::vector<int> obj_cpy(obj);
+    //{5, 7, 4, 6, 5, 4};
+    //finding 4;
+    std::cout << "looking for \"4\" in vector copy {5 7 4 6 5 4} : ";
+    try
+    {
+      if (&(easyfind<std::vector<int> >(obj_cpy, 4)) == &(obj_cpy.at(2)))
         std::cout << "found it !\n";
       else
         std::cout << "wrong reference returned !\n";
@@ -36,7 +51,7 @@ int main()
     obj.push_back(6);
     //{5, 6, 4, 6, 6};
     //finding 7;
-    std::cout << "looking for "7" in list {5 6 4 6 6} : ";
+    std::cout << "looking for \"7\" in deque {5 6 4 6 6} : ";
     try
     {
       std::cout << easyfind<std::deque<int> >(obj, 7) << "returned !\n";
@@ -48,6 +63,15 @@ int main()
   }
   {
     std::list<int> obj;
+    std::cout << "looking for \"5\" in empty list {} : ";
+    try
+    {
+        std::cout << easyfind<std::list<int> >(obj, 5) << "returned\n";
+    }
+    catch (std::exception &e)
+    {
+      std::cout << "couldn't find that\n";
+    }
     obj.push_back(5);
     obj.push_back(6);
     obj.push_back(4);
@@ -55,10 +79,10 @@ int main()
     obj.push_back(7);
     //{5, 6, 4, 5, 7};
     //finding 5;
-    std::cout << "looking for "5" in list {5 6 4 6 5 7} : ";
+    std::cout << "looking for \"5\" in list {5 6 4 6 5 7} : ";
     try
     {
-      if (&(easyfind<std::list<int> >(obj, 17)) == &(obj.front()))
+      if (&(easyfind<std::list<int> >(obj, 5)) == &(obj.front()))
         std::cout << "found it !\n";
       else
         std::cout << "wrong reference returned !\n";
